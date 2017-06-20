b9b37c5b0a9fb7730d541161f5cb8d34f67425af
Andrew Dowell <adowellphoto@gmail.com>
2017-06-20 13:04:55 +0100

    Removed logger and isListenerListening

--- src/main/java/com/company/widen/NativeRobot.java
+++ src/main/java/com/company/widen/NativeRobot.java
@@ -12,6 +12,9 @@ import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 public class NativeRobot implements NativeKeyListener {
     private String lineContext;
@@ -22,30 +25,19 @@ public class NativeRobot implements NativeKeyListener {
     private int endPosition = 0;
     private int initialPosition = 0;
     private int widenInvokeCount = 0;
-    private boolean listenerShouldBeListening = true; //PROBABLY GOING TO HAVE PROBLEMS WITH PARALLEL THREADS
 
     NativeRobot() {
         setupRobot();
         registerNativeHook();
     }
 
-    private synchronized void setContext() {
-        removeListener();
+    private void setContext() {
         selectEnd();
         this.startingPositionContext = copySelection();
         right();
         selectHome();
         this.lineContext = copySelection();
         left();
-        addListener();
-    }
-
-    private void addListener() {
-        this.listenerShouldBeListening = true;
-    }
-
-    private void removeListener() {
-        this.listenerShouldBeListening = false;
     }
 
     private void setupRobot() {
@@ -60,7 +52,7 @@ public class NativeRobot implements NativeKeyListener {
         robot.delay(2000);
     }
 
-    private synchronized void widen() {
+    private void widen() {
         int lineLength = lineContext.length();
         initialPosition = lineLength - startingPositionContext.length();
         List<String> chars = Arrays.asList(lineContext.split(""));
@@ -125,26 +117,26 @@ public class NativeRobot implements NativeKeyListener {
         robot.keyRelease(KeyEvent.VK_RIGHT);
     }
 
-    private synchronized void selectEnd() {
+    private void selectEnd() {
         robot.keyPress(KeyEvent.VK_SHIFT);
         robot.keyPress(KeyEvent.VK_END);
         robot.keyRelease(KeyEvent.VK_SHIFT);
         robot.keyRelease(KeyEvent.VK_END);
     }
 
-    private synchronized void selectHome() {
+    private void selectHome() {
         robot.keyPress(KeyEvent.VK_SHIFT);
         robot.keyPress(KeyEvent.VK_HOME);
         robot.keyRelease(KeyEvent.VK_SHIFT);
         robot.keyRelease(KeyEvent.VK_HOME);
     }
 
-    private synchronized void left() {
+    private void left() {
         robot.keyPress(KeyEvent.VK_LEFT);
         robot.keyRelease(KeyEvent.VK_LEFT);
     }
 
-    private synchronized void startOfLine() {
+    private void startOfLine() {
         robot.keyPress(KeyEvent.VK_END);
         robot.keyRelease(KeyEvent.VK_END);
         robot.keyPress(KeyEvent.VK_HOME);
@@ -154,8 +146,6 @@ public class NativeRobot implements NativeKeyListener {
     /* NATIVE KEY EVENTS */
 
     public void nativeKeyPressed(NativeKeyEvent e) {
-        System.out.println("listenerShouldBeListening = " + listenerShouldBeListening);
-        if (this.listenerShouldBeListening) {
             System.out.println("e.getKeyCode() = " + e.getKeyCode());
             if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
                 try {
@@ -173,7 +163,6 @@ public class NativeRobot implements NativeKeyListener {
                 System.out.println("Releasing context...");
                 releaseContext();
             }
-        }
     }
 
     private boolean noContext() {
@@ -201,11 +190,14 @@ public class NativeRobot implements NativeKeyListener {
         try {
             GlobalScreen.registerNativeHook();
             GlobalScreen.addNativeKeyListener(this);
+
+            LogManager.getLogManager().reset();
+            Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+            logger.setLevel(Level.OFF);
         } catch (NativeHookException ex) {
-            System.err.println("There was a problem registering the native hook.");
+            System.err.println("There was a problem registering the native hook and setting the logger to OFF.");
             System.err.println(ex.getMessage());
         }
-        addListener();
     }
 
 }
